55b4d04c86c21d601472beb0402b38f6a7ef0d84
Alena Kazeka <elenakoseko@gmail.com>
2020-10-15 18:49:12 +0300

    Configure project structure

---
+++ TennisApp/TennisApp/App/TennisAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  TennisAppApp.swift
+//  TennisApp
+//
+//  Created by Alena on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct TennisAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
